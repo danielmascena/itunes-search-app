461472c2bb522434621f8f6cf43d1166b1b3d534
Daniel Mascena <daniel.mascena@tripleplay.tv>
2022-04-08 17:27:38 +0100

    feat: create empty case component
    Add a component to being render in the case the result return zero occurence for a specific search. Issue: #20

--- frontend/src/components/MediaResourceList/index.tsx
+++ frontend/src/components/MediaResourceList/index.tsx
@@ -1,23 +1,26 @@
-import React from "react";
+import * as React from 'react';
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { Dispatch } from "redux";
 import InfiniteScroll from "react-infinite-scroll-component";
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import * as api from "../../service";
 import MediaResourceItem from "../MediaResourceItem";
+import ResultNotFound from "../ResultsNotFound";
 
 interface Props {
-    resources: IMedia[];
+    resources: Media[];
 }
 
 const MediaResourceList: React.FC<Props> = React.memo(({resources = []}: Props) => {
 
     const [collection, setCollection] = React.useState(resources);
     const fetchData = React.useCallback(() => {
-        //api.getMediaResourceArtistByTerm('iron maiden')
-        api.getMediaResourceAlbumByTerm('powerslave')
+        api.getMediaResourceArtistByTerm('iron maiden')
+        //api.getMediaResourceAlbumByTerm('british steel')
         .then(response => {
             if (response.status === 200) {
                 setCollection([...collection, ...response.data.results])
@@ -26,7 +29,7 @@ const MediaResourceList: React.FC<Props> = React.memo(({resources = []}: Props) 
     }, [collection]);
 
     React.useEffect(() => {
-        if (collection.length < 20) {
+        if (collection.length > 0 && collection.length <= 10) {
             fetchData();
         }
     }, [fetchData, collection.length]);
@@ -34,21 +37,30 @@ const MediaResourceList: React.FC<Props> = React.memo(({resources = []}: Props) 
     return (
         <section>
             <h2>List of Resources</h2>
-            <List>
-                <InfiniteScroll
-                    dataLength={collection.length}
-                    next={fetchData}
-                    hasMore={true}
-                    loader={<h4>Loading...</h4>}
-                >
-                    {collection.map((resource: IMedia) => (
-                        <>
-                            <MediaResourceItem media={resource} />
-                            <Divider variant="inset" component="li" />
-                        </>
-                    ))}
-                </InfiniteScroll>
-            </List>
+            {resources.length 
+                ? (
+                    <List>
+                        <InfiniteScroll
+                            dataLength={collection.length}
+                            next={fetchData}
+                            hasMore={true}
+                            loader={<Box sx={{ width: '100%', textAlign: 'center' }}>
+                                    <CircularProgress disableShrink />
+                                </Box>
+                            }
+                        >
+                            {collection.map((resource: Media) => (
+                                <>
+                                    <MediaResourceItem media={resource} />
+                                    <Divider variant="inset" component="li" />
+                                </>
+                            ))}
+                        </InfiniteScroll>
+                    </List>
+                ) : (
+                    <ResultNotFound />
+                )
+            }
         </section>
     );
 });
